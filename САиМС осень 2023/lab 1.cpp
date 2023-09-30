c655b34f66624390aacc099857ba288091dfb8da
esteemeran <76672853+esteemeran@users.noreply.github.com>
2023-09-30 16:20:24 +1000

    Update lab 1.cpp

--- САиМС осень 2023/lab 1.cpp
+++ САиМС осень 2023/lab 1.cpp
@@ -24,7 +24,7 @@ void init_graph(std::string fname, std::map<int,std::vector<int>> &g)
     {
         in>>init>>fin;
         g[init].push_back(fin);//добавляем fin в список смежности узла init
-        g[fin]; //конец дуги тоже узел. (появится, если не было, ничего не случится, если был.)
+      //  g[fin]; //конец дуги тоже узел. (появится, если не было, ничего не случится, если был.)
     }
     in.close();
 }
@@ -35,25 +35,27 @@ bool isFound(std::vector<int> &p, int f){
 		if(v==f){r=true; break;}
 	return r;
 }
-void foo(int init_node, int fin_node, std::map<int,std::vector<int>> &graph, std::string path, bool base,std::vector<int> &p)
+void foo(int init_node, int fin_node, std::map<int,std::vector<int>> &graph, std::string path,std::vector<int> &p)
 {
-    if (init_node == fin_node || (base && isFound(p, init_node)))
+    path += std::to_string(init_node) + " "; // добавляем в путь текущую ноду
+ 		p.push_back(init_node);
+		if (init_node == fin_node)
     {
         //путь найден, печатаем и возвращаемся.        
         std::cout<<path<<std::endl;
         return;
     }
- 
-    path += std::to_string(init_node) + " "; // добавляем в путь текущую ноду
- 		p.push_back(init_node);
-		    
+        
 		if (graph[init_node].empty() )
         return; // Это сток графа, путь не найден
- 
+ 		if(isFound(p, init_node)&&(!p.back()==init_node)){
+			cout<<path<<" cirlce"<<endl;
+			return;
+		}
     //рекурсивно продолжаем поиск для дочерних нод 
     for (auto subnode:graph[init_node])
     {
-    		foo(subnode,fin_node,graph,path, false,p);
+    		foo(subnode,fin_node,graph,path,p);
     }
     return;
 }
@@ -71,9 +73,9 @@ void show(std::map<int,std::vector<int>> &graph)
 }
 
 //граф из битмап в пары
-void bitmapConverter()
+void bitmapConverter(string fname)
 {
-	std::string fname = "./d1.txt", res="";
+	std::string res="";
 	std::ifstream in(fname,std::ifstream::in);
     int n , t=0, c=0, r=1, ca=0;
     in >> n; // тут просто выкидываем строки с количеством узлов  
@@ -92,13 +94,15 @@ int main()
 {
     
 		std::map<int,std::vector<int>> graph;
-    std::string fname = "./data.txt";
- 
+		std::map<int,std::vector<int>> length;
+    
     //читаем дуги из фаила
-    init_graph(fname,graph);
-		show(graph);
-	
-    //спрашиваем о пути
+    init_graph("./data.txt",graph);
+		init_graph("./d1.txt",length);
+		//show(graph);
+		show(length);
+	 
+	 	/*//спрашиваем о пути
     int start, end;
     std::cout<<"узлы начала и конца через пробел: ";
     std::cin>>start>>end;
@@ -107,8 +111,17 @@ int main()
     std::string path = "-->: ";
 		vector <int> p;
     foo(start,end,graph,path, true, p);
-    std::cout<<"все варианты проверены"<<std::endl;
-		
-		//bitmapConverter();
+    std::cout<<"все варианты проверены"<<std::endl;*/
+
+		//auto far = length[4];
+		for(auto i:length[4])
+			for(auto j:length[1]){
+				int start = i, end = j;
+				std::string path = "-->: ";
+				vector <int> p;
+    		foo(start,end,graph,path, p);
+			}
+	
+		//bitmapConverter("./d1.txt");
     return 0;
 }
